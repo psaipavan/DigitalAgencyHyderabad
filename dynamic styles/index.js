e4b7e6b226671f3e19fa3039eda8c8b695109282
psaipavan <saipavanpulluri07@gmail.com>
2025-06-10 16:52:03 +0530

    update the sections

--- dynamic styles/index.js
+++ dynamic styles/index.js
@@ -1,3 +1,25 @@
-let a;
-let b;
-console.log(a==b)
+odd_sum=0;
+even_sum=0;
+for(let i=1;i<=10;i++)
+{
+    if(i%2!=0){
+        odd_sum+=i;
+    }
+    else if(i%2==0){
+        even_sum+=i;
+    }
+}
+console.log(odd_sum);
+console.log(even_sum);
+
+
+
+let i=1;
+while (i<=5)
+{
+    console.log(i);
+    i+=1;
+}
+
+
+
