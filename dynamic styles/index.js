d23a76ced2b2d1c94901d22a652164438604031a
psaipavan <saipavanpulluri07@gmail.com>
2025-06-13 14:46:04 +0530

    name to hivitrax

--- dynamic styles/index.js
+++ dynamic styles/index.js
@@ -1,25 +1,24 @@
-odd_sum=0;
-even_sum=0;
-for(let i=1;i<=10;i++)
-{
-    if(i%2!=0){
-        odd_sum+=i;
-    }
-    else if(i%2==0){
-        even_sum+=i;
-    }
-}
-console.log(odd_sum);
-console.log(even_sum);
+// odd_sum=0;
+// even_sum=0;
+// for(let i=1;i<=10;i++)
+// {
+//     if(i%2!=0){
+//         odd_sum+=i;
+//     }
+//     else if(i%2==0){
+//         even_sum+=i;
+//     }
+// }
+// console.log(odd_sum);
+// console.log(even_sum);
 
 
 
-let i=1;
-while (i<=5)
-{
-    console.log(i);
-    i+=1;
-}
-
+// let i=1;
+// while (i<=5)
+// {
+//     console.log(i);
+//     i+=1;
+// }
 
 
